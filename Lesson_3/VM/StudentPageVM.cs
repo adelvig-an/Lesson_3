5ef03333df42090467c4878229a19c1ba9fb517b
adelvig-an <63202963+adelvig-an@users.noreply.github.com>
2020-06-23 18:57:22 +0700

    Lesson 3

--- Lesson_3/VM/StudentPageVM.cs
+++ Lesson_3/VM/StudentPageVM.cs
@@ -6,6 +6,8 @@ using System.ComponentModel;
 using System.Globalization;
 using System.Text;
 using System.Windows;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
 
 namespace Lesson_3.VM
 {
@@ -34,6 +36,57 @@ namespace Lesson_3.VM
                     YearUniversity = Student.YearUniversity
                 }
             };
+            AddStudent = new RelayCommand(_ => AddStudentAction());
+        }
+
+        private string text;
+        public string MiddleNameText
+        {
+            get => text;
+            set => SetProperty(ref text, value);
+        }
+        public string FirstNameText
+        {
+            get => text;
+            set => SetProperty(ref text, value);
+        }
+        public string LastNameText
+        {
+            get => text;
+            set => SetProperty(ref text, value);
+        }
+        public string YearUniversityText
+        {
+            get => text;
+            set => SetProperty(ref text, value);
+        }
+
+        private DateTime dateTimeText;
+        public DateTime DateTimeText
+        {
+            get => dateTimeText;
+            set => SetProperty(ref dateTimeText, value);
+        }
+
+        //public int AgeResalt
+        //{
+        //    get
+        //    {
+        //        int a;
+        //        a = Convert.ToString(DateTimeText.DateTime);
+        //        var date = DateTime.ParseExact(a, "dd.MM.yyyy", CultureInfo.InvariantCulture);
+        //        var age = DateTime.Now.Year - date.Year;
+        //        if (DateTime.Now.Month < date.Month ||
+        //           (DateTime.Now.Month == date.Month && DateTime.Now.Day < date.Day)) age--;
+                
+        //        return age;
+        //    }
+        //}
+
+        public ICommand AddStudent { get; }
+        public void AddStudentAction()
+        {
+            StudentList.Add(new Student());
         }
     }
 }
